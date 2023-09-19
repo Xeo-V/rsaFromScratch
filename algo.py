eb5ba5737b1afe6c41fb58081c62d04626e9aeaa
X30 <74484800+Xeo-V@users.noreply.github.com>
2023-09-20 00:21:30 +0530

    Update algo.py
    -fixed issues with parallization

--- algo.py
+++ algo.py
@@ -12,7 +12,13 @@ def mod_inv(a, m):
             return i
     return None
 
-
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 def generate_prime_candidate(length):
     p = random.getrandbits(length)
@@ -25,72 +31,49 @@ def generate_prime_number(length=1024):
         p = generate_prime_candidate(length)
     return p
 
-def funrandom gen 
 def generate_keypair(p, q):
-    try:
-        n = p * q
-        phi = (p - 1) * (q - 1)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = random.randrange(2, phi)
+    g = gcd(e, phi)
+    while g != 1:
         e = random.randrange(2, phi)
         g = gcd(e, phi)
-
-        while g != 1:
-            e = random.randrange(2, phi)
-            g = gcd(e, phi)
-
-        d = mod_inv(e, phi)
-        if d is None:
-            raise ValueError("Modular inverse does not exist.")
-        return ((e, n), (d, n))
-    except Exception as e:
-        print(f"Error: {e}")
-        return None, None
+    d = mod_inv(e, phi)
+    return ((e, n), (d, n))
 
 def encrypt(public_key, plaintext):
-    try:
-        e, n = public_key
-        cipher = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future_to_char = {executor.submit(lambda p: (ord(p) ** e) % n, c): c for c in plaintext}
-            for future in concurrent.futures.as_completed(future_to_char):
-                cipher.append(future.result())
-        return cipher
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    e, n = public_key
+    cipher = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_char = [executor.submit(lambda p: (ord(p) ** e) % n, c) for c in plaintext]
+    cipher = [future.result() for future in future_to_char]
+    return cipher
 
 def decrypt(private_key, ciphertext):
-    try:
-        d, n = private_key
-        plain = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future_to_char = {executor.submit(lambda p: chr((p ** d) % n), c): c for c in ciphertext}
-            for future in concurrent.futures.as_completed(future_to_char):
-                plain.append(future.result())
-        return ''.join(plain)
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    d, n = private_key
+    plain = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_char = [executor.submit(lambda p: chr((p ** d) % n), c) for c in ciphertext]
+    plain = [future.result() for future in future_to_char]
+    return ''.join(plain)
 
 if __name__ == '__main__':
-    try:
-        length = int(input("Enter bit length for p and q (e.g., 8, 16, 32): "))
-        p = generate_prime_number(length)
-        q = generate_prime_number(length)
+    length = int(input("Enter bit length for p and q (e.g., 8, 16, 32): "))
+    p = generate_prime_number(length)
+    q = generate_prime_number(length)
 
-        print(f"p = {p}, q = {q}")
+    print(f"p = {p}, q = {q}")
 
-        public, private = generate_keypair(p, q)
+    public, private = generate_keypair(p, q)
 
-        if public and private:
-            print(f"Public key: {public}")
-            print(f"Private key: {private}")
+    print(f"Public key: {public}")
+    print(f"Private key: {private}")
 
-            text = input("Enter text to encrypt: ")
+    text = input("Enter text to encrypt: ")
 
-            encrypted_msg = encrypt(public, text)
-            print(f"Encrypted: {encrypted_msg}")
+    encrypted_msg = encrypt(public, text)
+    print(f"Encrypted (as list of integers): {encrypted_msg}")
 
-            decrypted_msg = decrypt(private, encrypted_msg)
-            print(f"Decrypted: {decrypted_msg}")
-    except Exception as e:
-        print(f"Error: {e}")
+    decrypted_msg = decrypt(private, encrypted_msg)
+    print(f"Decrypted: {decrypted_msg}")
